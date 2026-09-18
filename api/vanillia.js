7fe0656858ba72185da1926bf91a0544435601ad
Blake181041 <Blakeryan1418@gmail.com>
2026-09-17 22:44:46 -0400

    Remove Vercel-specific VanilliaPXY routing

--- api/vanillia.js
+++
@@ -1,8 +0,0 @@
-import { handleRequest } from "vanilliapxy";
-
-export default function handler(req, res) {
-  const url = new URL(req.url || "/", `http://${req.headers.host || "localhost"}`);
-  url.pathname = "/vanillia";
-  req.url = url.pathname + url.search;
-  return handleRequest(req, res);
-}
